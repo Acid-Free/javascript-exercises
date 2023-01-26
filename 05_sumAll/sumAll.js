fdc12fd839934d1fe73daccfb0c7e633447db7ab
Sian Caleb Tolentino Domasig <49774633+Acid-Free@users.noreply.github.com>
2023-01-26 14:35:49 +0800

    Finish activity 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  // Non-number input is invalid
+  if (typeof start !== "number" || typeof end !== "number") return "ERROR";
 
+  // Negative input is invalid
+  if (start < 0 || end < 0) return "ERROR";
+
+  // Start should not be larger than end
+  if (start > end) {
+    const temp = start;
+    start = end;
+    end = temp;
+  }
+
+  let result = start;
+  for (let i = start + 1; i <= end; ++i) {
+    result += i;
+  }
+  return result;
 };
 
 // Do not edit below this line
