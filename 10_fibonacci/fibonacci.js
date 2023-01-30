d276faad7eb733c381ee626a07f0cc8a11cad8d3
Sian Caleb <49774633+Acid-Free@users.noreply.github.com>
2023-01-30 12:34:01 +0800

    Finish 10 fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function (input) {
+  input = parseInt(input);
+  if (!input) return "OOPS";
+  if (input < 0) return "OOPS";
 
+  let prev = 1;
+  let current = 1;
+
+  for (let i = 2; i < input; ++i) {
+    const temp = current;
+    current += prev;
+    prev = temp;
+  }
+
+  return current;
 };
 
 // Do not edit below this line
