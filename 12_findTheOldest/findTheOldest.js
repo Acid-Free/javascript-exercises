8469d47b173c5d8ac31c229902f4394dbdd474b1
Sian Caleb <49774633+Acid-Free@users.noreply.github.com>
2023-01-30 12:57:47 +0800

    Finish 12 findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currentYear = new Date().getFullYear();
+  const oldest = people.sort((a, b) => {
+    const ageA =
+      ("yearOfDeath" in a ? a.yearOfDeath : currentYear) - a.yearOfBirth;
+    const ageB =
+      ("yearOfDeath" in b ? b.yearOfDeath : currentYear) - b.yearOfBirth;
 
+    return ageB - ageA;
+  })[0];
+
+  return oldest;
 };
 
 // Do not edit below this line
