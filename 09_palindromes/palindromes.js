2fec4e193ae4bc72f8f1e55a290fd51a94a0c06c
Sian Caleb <49774633+Acid-Free@users.noreply.github.com>
2023-01-30 12:07:11 +0800

    Finish 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (stringInput) {
+  stringInput = stringInput.toLowerCase();
+  stringInput = [...stringInput].filter((char) => isLetter(char)).join("");
+  const reversed = stringInput.split("").reverse().join("");
+  return stringInput === reversed;
+};
 
+const isLetter = (char) => {
+  return /[a-z]/.test(char);
 };
 
 // Do not edit below this line
